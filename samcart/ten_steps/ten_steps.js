ce702ef8d4e2e31c0b360cbb3e2249fb17b0957f
Svetlana Khvastova <svetakhvastova@gmail.com>
2021-10-13 17:42:25 +0300

    fix

--- samcart/ten_steps/ten_steps.js
+++ samcart/ten_steps/ten_steps.js
@@ -1,3 +1,4 @@
+// click on 3d cart
 document.querySelector('.box_fist_about > div > .scene form > button').addEventListener('click', function (e) {
   e.preventDefault()
   document.querySelector('.card').classList.toggle('is-flipped');
@@ -8,29 +9,32 @@ document.querySelector('.box_fist_about > div > .scene form > span').addEventLis
   document.querySelector('.card').classList.toggle('is-flipped');
 });
 
-// document.querySelector('.box_already_registered form button').addEventListener('click', function (e) {
-//   e.preventDefault()
-//   showModal(".backdrop_popup.popup_btn")
-// });
-
-document.querySelector('.box_already_registered form button').addEventListener('click', function (e) {
+// click btn for popup
+document.querySelector('.box_already_registered button').addEventListener('click', function (e) {
   e.preventDefault()
-  showModal(".backdrop_popup.popup_after_scroll")
+  showModal(".backdrop_popup")
 });
-  
 
-document.querySelector(".backdrop_popup .popup_form_content .close_popup").addEventListener("click", function (e) {
-  e.preventDefault()
-  document.querySelector(".backdrop_popup").classList.add("is_hidden")
-  document.body.style.overflow = ""
+// click close popup
+document.querySelectorAll(".close_popup").forEach((item) => {  
+  item.addEventListener("click", function () {
+    document.querySelector(".backdrop_popup").classList.add("is_hidden")
+    document.body.style.overflow = ""
+
+    setTimeout(() => {
+      document.querySelector('.backdrop_popup .popup_after_scroll').style.display = "block"
+    }, 1000)
+  })
 })
+
 
 function showModal(selector) {
   document.querySelector(selector).classList.remove("is_hidden")
   document.body.style.overflow = "hidden"
+  document.querySelector('.backdrop_popup .popup_after_scroll').style.display = "none"
 }
 
-
+// slider
 let slider = tns(
 {
   container: ".my-slider",
@@ -38,7 +42,25 @@ let slider = tns(
   navPosition: 'bottom',
   prevButton: ".arrow_left",
   nextButton: ".arrow_right"
-})
+  })
+
+// send form
+// document.querySelector('.box_fist_about > div > .scene form > button').addEventListener('click', () => {
+//   document.querySelector('.button.button--color--primary').click()
+// })
+
+// document.querySelector('.popup_wrapper div:last-child button').addEventListener('click', () => {
+//   document.querySelector('.button.button--color--primary').click()
+// })
+
+// document.querySelector('.backdrop_popup.popup_after_scroll button:last-child').addEventListener('click', () => {
+//   document.querySelector('.button.button--color--primary').click()
+// })
+
+
+  
+
+
 
 
 
