b79375adc83305128bd3ae1c2e655f687462640d
Svetlana Khvastova <svetakhvastova@gmail.com>
2021-10-18 18:11:31 +0300

    fix

--- samcart/ten_steps/ten_steps.js
+++ samcart/ten_steps/ten_steps.js
@@ -12,8 +12,17 @@ document.querySelector('.box_fist_about > div > .scene form > span').addEventLis
 // click btn for popup
 document.querySelector('.box_already_registered button').addEventListener('click', function (e) {
   e.preventDefault()
-  showModal(".backdrop_popup")
+  document.querySelector(".backdrop_popup").classList.remove("is_hidden")
+  document.body.style.overflow = "hidden"
+  document.querySelector('.backdrop_popup .popup_after_scroll').style.display = "none"
 });
+
+document.querySelector('.first_mobile_btn').addEventListener('click', (e) => {
+  e.preventDefault()
+  document.querySelector(".backdrop_popup").classList.remove("is_hidden")
+  document.body.style.overflow = "hidden"
+  document.querySelector('.backdrop_popup .popup_btn').style.display = "none"
+})
 
 // click close popup
 document.querySelectorAll(".close_popup").forEach((item) => {  
@@ -23,21 +32,15 @@ document.querySelectorAll(".close_popup").forEach((item) => {
 
     setTimeout(() => {
       document.querySelector('.backdrop_popup .popup_after_scroll').style.display = "block"
-    }, 1000)
+      document.querySelector('.backdrop_popup .popup_btn').style.display = "block"
+    }, 100)
   })
 })
-
-
-function showModal(selector) {
-  document.querySelector(selector).classList.remove("is_hidden")
-  document.body.style.overflow = "hidden"
-  document.querySelector('.backdrop_popup .popup_after_scroll').style.display = "none"
-}
 
 // slider
 let slider = tns(
 {
-  container: ".my-slider",
+  container: ".my_slider",
   items: 1,
   navPosition: 'bottom',
   prevButton: ".arrow_left",
@@ -58,7 +61,9 @@ let slider = tns(
 // })
 
 
-  
+setTimeout(() => {
+    document.querySelector('.backdrop_popup.is_hidden').style.display = "block"
+  }, 100)
 
 
 
