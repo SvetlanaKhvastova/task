5212dd7af6332464a6c7074f6ede0603de86b78d
Svetlana Khvastova <svetakhvastova@gmail.com>
2022-01-21 19:01:54 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -8,3 +8,15 @@ let scriptCustom = document.createElement("script")
 scriptCustom.src = "https://conversionratestore.github.io/projects/knineti/new_page.js"
 scriptCustom.async = false
 document.head.appendChild(scriptCustom)
+
+//
+document.head.insertAdjacentHTML("beforeend", `<style class="exp">body {opacity: 0;}</style>`)
+
+let scriptCustom = document.createElement("script")
+scriptCustom.src = "https://conversionratestore.github.io/projects/knineti/new_page.js"
+scriptCustom.async = false
+document.head.appendChild(scriptCustom)
+
+setTimeout(function () {
+  document.querySelector(".exp")?.remove()
+}, 5000)
