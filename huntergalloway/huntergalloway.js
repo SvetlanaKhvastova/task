5513c0e1e52901e941c245519e54ddf0bfb73c07
Svetlana Khvastova <svetakhvastova@gmail.com>
2022-05-09 22:01:25 +0300

    hunter

---
+++ huntergalloway/huntergalloway.js
@@ -0,0 +1,45 @@
+scrolling(".second_block > ol li > a")
+
+// js scrolling
+function scrolling(upSelector) {
+  let links = document.querySelectorAll(upSelector),
+    speed = 0.4
+
+  links.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault()
+
+      let widthTop = document.documentElement.scrollTop,
+        hash = this.hash,
+        toBlock = document.querySelector(hash).getBoundingClientRect().top - 100,
+        start = null
+
+      if (window.innerWidth <= 768) {
+        toBlock = document.querySelector(hash).getBoundingClientRect().top - 70
+      }
+
+      requestAnimationFrame(step)
+
+      function step(time) {
+        if (start === null) {
+          start = time
+        }
+
+        let progress = time - start,
+          r = toBlock < 0 ? Math.max(widthTop - progress / speed, widthTop + toBlock) : Math.min(widthTop + progress / speed, widthTop + toBlock)
+
+        document.documentElement.scrollTo(0, r)
+
+        if (r != widthTop + toBlock) {
+          requestAnimationFrame(step)
+        } else {
+        }
+        location.hash = hash
+      }
+    })
+  })
+}
+
+if (window.innerWidth <= 768) {
+  document.querySelector(".third_block > ol > li:nth-child(8) > div .img_wrap").after(document.querySelector(".third_block > ol > li:nth-child(8) a.link_more_articles"))
+}
