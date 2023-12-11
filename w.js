3c6d1bbb4c9d3cebefe109693842d8d75a9b48ef
Svetlana Khvastova <svetakhvastova@gmail.com>
2023-12-11 18:08:18 +0200

    fix

--- w.js
+++
@@ -1,6 +0,0 @@
-let lookForEarn = setInterval(() => {
-  if (document.querySelector(".product__price:not(.product__price--compare) .money").getAttribute("doubly-currency-usd")) {
-    clearInterval(lookForEarn);
-    handleKlarna();
-  }
-}, 500);
