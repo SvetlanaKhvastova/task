db4cdd9f4bc44118616fa6cec8f9842c68b0be6a
Svetlana Khvastova <svetakhvastova@gmail.com>
2024-07-08 19:18:37 +0300

    fix

--- cerebelly/cart_checkout_hyp_3/blocks.ts
+++ cerebelly/cart_checkout_hyp_3/blocks.ts
@@ -28,12 +28,101 @@ export const flavorGuaranteeBlock = /* HTML */ `
 
 export const stickyBlock = /* HTML */ `
   <div class="sticky_block">
-    <div class="total_box">
-      <p>Order total</p>
-      <span>$11.98</span>
-    </div>
-    <button class="proceed_to_checkout_btn">Proceed to checkout</button>
+    <div class="total_box"></div>
+    <button class="proceed_to_checkout_btn">Proceed to checkout ${svg.arrowWhiteicon}</button>
   </div>
 `
 
 // Checkout page:
+export const orderSummaryBlock = /* HTML */ `
+  <div class="order_summary_block">
+    <ul class="products_list"></ul>
+  </div>
+`
+export const productItem = (
+  img: string,
+  title: string,
+  itemsCount: string,
+  cadence: string,
+  price: string,
+  subscribeBoxPercent: number
+) => {
+  return /* HTML */ `
+    <li class="products_item">
+      <div class="img_wrapper">
+        <img src=${img} alt="photo" />
+      </div>
+      <div class="item_info">
+        <div>
+          <h3>${title}</h3>
+          <div class="quantity_packs_info">
+            <span class="items_count">${itemsCount}</span>
+            <span>|</span>
+            <span class="cadence_txt">${cadence}</span>
+          </div>
+          ${subscribeBoxPercent === 15 ? `<div class="subscribe_txt">Subscribe & Save 15%</div>` : ''}
+        </div>
+        <div class="price_wrapper">
+          ${subscribeBoxPercent === 15
+            ? `<span class="price_txt green_color">${price}</span>`
+            : `<span class="price_txt">${price}</span>`}
+        </div>
+      </div>
+    </li>
+  `
+}
+
+export const payNowTxtBlock = /* HTML */ `
+  <div class="pay_now_txt_block">
+    <p>
+      By clicking "Pay now", you agree to our
+      <a
+        class="terms_sale_link"
+        href="http://https://cerebelly.com/terms-sale"
+        target="_blank"
+        rel="noopener noreferrer"
+        >Terms and Conditions</a
+      >
+      and
+      <a class="privacy_link" href="https://cerebelly.com/privacy" target="_blank" rel="noopener noreferrer"
+        >Privacy Policy</a
+      >.
+    </p>
+  </div>
+`
+
+export const newShippingBlock = (standartShipping: string, expeditedShipping: string) => {
+  return /* HTML */ `
+    <div class="new_shipping_block">
+      <input class="custom_radio" value="" type="radio" name="shipping" id="standartShippingVar" />
+      <label for="standartShippingVar">
+        <div class="radio_info">
+          <div class="radio_txt_box">
+            <span class="new_custom_radio"></span>
+            <span class="radio_txt">Standart shipping</span>
+            <span class="shipping_txt accent_color_green">${standartShipping}</span>
+          </div>
+        </div>
+        <div class="radio_days">
+          <span>5-7 days</span>
+          <img src="${git}shipping_img.png" alt="shipping img" />
+        </div>
+      </label>
+
+      <input class="custom_radio" value="" type="radio" name="shipping" id="expeditedShippingVar" />
+      <label for="expeditedShippingVar">
+        <div class="radio_info">
+          <div class="radio_txt_box">
+            <span class="new_custom_radio"></span>
+            <span class="radio_txt">Expedited shipping</span>
+            <span class="shipping_txt">${expeditedShipping}</span>
+          </div>
+        </div>
+        <div class="radio_days">
+          <span>1-3 days</span>
+          <img src="${git}shipping_img.png" alt="shipping img" />
+        </div>
+      </label>
+    </div>
+  `
+}
--- cerebelly/cart_checkout_hyp_3/index.ts
+++ cerebelly/cart_checkout_hyp_3/index.ts
@@ -8,7 +8,15 @@ import {
   visibilityOfTime,
   loadScriptsOrStyles
 } from '../../libraries'
-import { freeShippingBlock, flavorGuaranteeBlock } from './blocks'
+import {
+  freeShippingBlock,
+  flavorGuaranteeBlock,
+  stickyBlock,
+  payNowTxtBlock,
+  orderSummaryBlock,
+  productItem,
+  newShippingBlock
+} from './blocks'
 import { svg } from './data'
 
 // @ts-ignore
@@ -18,15 +26,20 @@ const device = window.innerWidth < 768 ? 'mobile' : 'desktop'
 
 class cartCheckoutPages {
   device: 'mobile' | 'desktop'
+  observer: null | MutationObserver
+  lastPath: string
 
   constructor(device) {
     this.device = device
+    this.observer = null
+    this.lastPath = window.location.pathname
     this.init()
+    this.observePageChange()
   }
 
   init() {
     console.dir("name: 'Cart + Checkout', dev: 'SKh")
-    startLog({ name: 'Cart + Checkout', dev: 'SKh' })
+    // startLog({ name: 'Cart + Checkout', dev: 'SKh' })
     // clarityInterval('')
     if (!$el('.crs_inter')) {
       document.head.insertAdjacentHTML(
@@ -34,56 +47,168 @@ class cartCheckoutPages {
         `<link class="crs_inter" href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">`
       )
     }
-    document.head.insertAdjacentHTML('beforeend', `<style>${mainStyle}</style>`)
-
-    if (window.location.pathname.match('cart')) {
-      this.renderFlavorGuaranteeBlock()
-      this.changedTxtBtnCheckout()
-      this.initTooltip()
+    if (!$el('.crs_style')) {
+      document.head.insertAdjacentHTML('beforeend', `<style class="crs_style">${mainStyle}</style>`)
+    }
+    // _________________________________________________________________________________________________________________
+    if (this.checkPage() === 'cart') {
+      if (!$el('.flavor_guarantee_block')) {
+        this.renderFlavorGuaranteeBlock()
+        this.initTooltip()
+      }
+      if (!$el('.actions-wrapper button')?.innerHTML.includes('Proceed to checkout')) {
+        this.changedTxtBtnCheckout()
+      }
       setTimeout(() => {
         this.changedTxtShipping()
       }, 1500)
+      if (this.device === 'mobile') {
+        if (!$el('.sticky_block')) {
+          this.renderStickyBlock()
+        }
+        this.renderTxtStickyBlock()
+        this.toggleStickyBlockVisibility()
+      }
     }
+    // _________________________________________________________________________________________________________________
+    if (this.checkPage() === 'checkout') {
+      if (!$el('.flavor_guarantee_block')) {
+        this.renderFlavorGuaranteeBlock()
+        this.initTooltip()
+      }
+
+      if (!$el('.order_summary_block')) {
+        this.renderOrderSummaryBlock()
+      }
+
+      if (!$el('.new_shipping_block')) {
+        this.replaceStandardFreeShippingBlock()
+      }
+      if (!$el('.pay_now_txt_block')) {
+        this.renderPayNowTxtBlock()
+      }
+
+      if (this.device === 'mobile') {
+        $el('.sticky_block')?.remove()
+      }
+    }
+  }
+
+  checkPage(): string {
+    const url = window.location.href
+    switch (true) {
+      case url.includes('/checkout'):
+        return 'checkout'
+      case url.includes('/cart'):
+        return 'cart'
+      default:
+        return 'other'
+    }
+  }
+
+  observePageChange() {
+    this.observer = new MutationObserver(mutations => {
+      mutations.forEach(mutation => {
+        if (mutation.target.nodeName === 'BODY') {
+          // _________________________________________________________________________________________________________________
+          if (this.checkPage() === 'cart') {
+            // console.dir(mutation, `mutation`)
+            if (!$el('.flavor_guarantee_block')) {
+              this.renderFlavorGuaranteeBlock()
+              this.initTooltip()
+            }
+            if (!$el('.actions-wrapper button')?.innerHTML.includes('Proceed to checkout')) {
+              this.changedTxtBtnCheckout()
+            }
+            setTimeout(() => {
+              this.changedTxtShipping()
+            }, 1500)
+
+            if (this.device === 'mobile') {
+              if (!$el('.sticky_block')) {
+                this.renderStickyBlock()
+                this.toggleStickyBlockVisibility()
+              }
+            }
+          }
+          // _________________________________________________________________________________________________________________
+          if (this.checkPage() === 'checkout') {
+            if (!$el('.flavor_guarantee_block')) {
+              this.renderFlavorGuaranteeBlock()
+              this.initTooltip()
+            }
+
+            if (!$el('.order_summary_block')) {
+              this.renderOrderSummaryBlock()
+            }
+            if (!$el('.new_shipping_block')) {
+              this.replaceStandardFreeShippingBlock()
+            }
+            if (!$el('.pay_now_txt_block')) {
+              this.renderPayNowTxtBlock()
+            }
+
+            if (this.device === 'mobile') {
+              $el('.sticky_block')?.remove()
+            }
+          }
+        }
+
+        // _________________________________________________________________________________________________________________
+        for (let node of mutation.addedNodes) {
+          if (!(node instanceof HTMLElement)) continue
+          // console.dir(node, `node`)
+          // _________________________________________________________________________________________________________________
+          if (this.checkPage() === 'cart') {
+            if (this.device === 'mobile') {
+              this.handleDisabledStickyBlock()
+
+              if (node.classList.contains('text-green')) {
+                this.renderTxtStickyBlock()
+              }
+            }
+          }
+          // _________________________________________________________________________________________________________________
+          if (this.checkPage() === 'checkout') {
+            this.handleDisabledRadioBlock()
+          }
+        }
+      })
+    })
+
+    const config = { childList: true, subtree: true }
+    this.observer.observe(document.body, config)
   }
 
   renderFlavorGuaranteeBlock() {
     waitForElement('.summary-table').then(() => {
+      let elemTrigger: string = ''
+      let place: string = ''
       if (!$el('.flavor_guarantee_block')) {
-        $el('.summary-table').insertAdjacentHTML('beforebegin', flavorGuaranteeBlock)
-      }
-    })
-  }
-
-  renderFreeShippingBlock() {
-    waitForElement('.flavor_guarantee_block').then(() => {
-      if (!$el('.free_shipping_block')) {
-        $el('.flavor_guarantee_block').insertAdjacentHTML('beforebegin', freeShippingBlock)
-      }
-    })
-  }
-
-  changedTxtBtnCheckout() {
-    waitForElement('.actions-wrapper button').then(() => {
-      const btn = $el('.actions-wrapper button')
-      if (btn && !btn.innerHTML.includes('Proceed to checkout')) {
-        btn.innerHTML = `Proceed to checkout ${svg.arrowWhiteicon} `
-      }
-    })
-  }
-  changedTxtShipping() {
-    waitForElement('.promo-code').then(() => {
-      $$el('.sum-row.total.coupon .caption').forEach(el => {
-        if (el.nextElementSibling.textContent === `Free`) {
-          this.renderFreeShippingBlock()
+        if (this.checkPage() === 'cart') {
+          if (this.device === 'mobile') {
+            elemTrigger = '.container-fluid .heading'
+            place = 'afterend'
+          } else {
+            elemTrigger = '.summary-table'
+            place = 'beforebegin'
+          }
         }
-        if (!el.textContent.includes('Subscription savings') && !el.textContent.includes('Standart shipping')) {
-          console.dir(el, `el`)
-          el.textContent = `Standart shipping`
+
+        if (this.checkPage() === 'checkout') {
+          if (this.device === 'mobile') {
+            // elemTrigger = '.summary-table'
+            // place = 'beforebegin'
+          } else {
+            elemTrigger = '.form-column-left'
+            place = 'afterbegin'
+          }
         }
-      })
+
+        $el(elemTrigger).insertAdjacentHTML(place, flavorGuaranteeBlock)
+      }
     })
   }
-
   initTooltip() {
     loadScriptsOrStyles([
       'https://unpkg.com/@popperjs/core@2.11.6/dist/umd/popper.min.js',
@@ -112,6 +237,196 @@ class cartCheckoutPages {
         }
       }, 100)
     })
+  }
+
+  renderFreeShippingBlock() {
+    waitForElement('.flavor_guarantee_block').then(() => {
+      if (!$el('.free_shipping_block')) {
+        $el('.flavor_guarantee_block').insertAdjacentHTML('beforebegin', freeShippingBlock)
+      }
+    })
+  }
+
+  // cart
+  changedTxtShipping() {
+    waitForElement('.promo-code').then(() => {
+      $$el('.sum-row.total.coupon .caption').forEach(el => {
+        if (el.nextElementSibling.textContent === `Free`) {
+          if (!$el('.free_shipping_block')) {
+            this.renderFreeShippingBlock()
+          }
+        } else {
+          $el('.free_shipping_block')?.remove()
+        }
+        if (
+          !el.textContent.includes('Subscription savings') &&
+          !el.textContent.includes('Standart shipping') &&
+          !el.closest('.total').previousElementSibling.classList.contains('sum-row-hr') &&
+          el.textContent !== 'Standart shipping'
+        ) {
+          el.textContent = `Standart shipping`
+        }
+      })
+    })
+  }
+  changedTxtBtnCheckout() {
+    waitForElement('.actions-wrapper button').then(() => {
+      const btn = $el('.actions-wrapper button')
+      if (btn && !btn.innerHTML.includes('Proceed to checkout')) {
+        btn.innerHTML = `Proceed to checkout ${svg.arrowWhiteicon}`
+      }
+    })
+  }
+  renderStickyBlock() {
+    waitForElement('#root').then(() => {
+      if (!$el('.sticky_block')) {
+        $el('#root').insertAdjacentHTML('afterbegin', stickyBlock)
+      }
+    })
+  }
+  renderTxtStickyBlock() {
+    waitForElement('.sticky_block').then(() => {
+      waitForElement('.sum-row.order-total .total').then(() => {
+        $el('.sticky_block .total_box').innerHTML = ''
+
+        let orderTotalElemClone = $el('.container-fluid .sum-row.order-total').cloneNode(true)
+        if (!$el('.sticky_block .total_box .sum-row.order-total')) {
+          $el('.sticky_block .total_box').insertAdjacentElement('afterbegin', orderTotalElemClone)
+        }
+      })
+    })
+  }
+  handleDisabledStickyBlock() {
+    if (!$el('.sticky_block .proceed_to_checkout_btn') && !$el('.actions-wrapper button')) return
+
+    if ($el('.actions-wrapper button')?.disabled) {
+      $el('.sticky_block .proceed_to_checkout_btn').disabled = true
+    } else {
+      $el('.sticky_block .proceed_to_checkout_btn').disabled = false
+    }
+  }
+  toggleStickyBlockVisibility() {
+    waitForElement('.actions-wrapper').then(() => {
+      const element = $el('.sticky_block') as HTMLElement
+      const elemClose = $el('.actions-wrapper') as HTMLElement
+
+      function visible() {
+        const options = {
+          root: null,
+          threshold: 0.5
+        }
+
+        let observerSticky = new IntersectionObserver(entries => {
+          entries.forEach(i => {
+            if (i.isIntersecting) {
+              element.style.display = 'none'
+            } else {
+              element.style.display = 'block'
+            }
+
+            observerSticky.unobserve(i.target)
+          })
+
+          observerSticky.disconnect()
+        }, options)
+
+        observerSticky.observe(elemClose)
+      }
+
+      window.addEventListener('scroll', () => {
+        visible()
+      })
+
+      visible()
+    })
+  }
+
+  // checkout
+  renderPayNowTxtBlock() {
+    waitForElement('.form-column-left button').then(() => {
+      if (!$el('.pay_now_txt_block')) {
+        $el('.form-column-left button').insertAdjacentHTML('afterend', payNowTxtBlock)
+      }
+    })
+  }
+  renderOrderSummaryBlock() {
+    waitForElement('.form-column-right').then(() => {
+      if (!$el('.order_summary_block')) {
+        console.dir('renderOrderSummaryBlock')
+        $el('.form-column-right .checokut-title-wrapper').insertAdjacentHTML('afterend', orderSummaryBlock)
+
+        waitForElement('.order_summary_block .products_list').then(() => {
+          if (!localStorage.getItem('v4Cart')) return
+          $el('.products_list').innerHTML = ''
+
+          let cartLocalStor = JSON.parse(localStorage.getItem('v4Cart'))
+          let products = cartLocalStor.cart.boxes
+          const currency = $el('.order-total .total')?.textContent.charAt(0)
+
+          if ($el('.products_list').children.length === products.length) return
+
+          for (const key in products) {
+            const img = products[key]?.image?.includes('https://cerebelly.com/wp-json/')
+              ? products[key].image
+              : `https://cerebelly.com/wp-json/cerebelly/image/get?path=${products[key].image}`
+            const title = products[key].title
+            const itemsCount = products[key].count
+            const cadence = products[key].cadence.includes('week')
+              ? `Every ${products[key].cadence}`
+              : 'One-time purchase'
+
+            const price = `${currency}${products[key].price.toFixed(2)}`
+            const subscribeBoxPercent = (
+              ((products[key].priceDefault - products[key].price) / products[key].priceDefault) *
+              100
+            ).toFixed(0)
+
+            if ($el('.products_list').children.length !== products.length) {
+              $el('.products_list').insertAdjacentHTML(
+                'beforeend',
+                productItem(img, title, itemsCount, cadence, price, +subscribeBoxPercent)
+              )
+            }
+          }
+          console.dir(products, 'products')
+        })
+      }
+    })
+  }
+  replaceStandardFreeShippingBlock() {
+    waitForElement('.sum-row.shipping.standard-shipping.active').then(() => {
+      const standartShipping = $$el('.sum-row.standard-shipping .total')[0].textContent
+      const expeditedShipping = $$el('.sum-row.standard-shipping .total')[1].textContent
+      if (!$el('.new_shipping_block')) {
+        $el('.stripe-inputs.checkout-block').previousElementSibling.insertAdjacentHTML(
+          'beforebegin',
+          newShippingBlock(standartShipping, expeditedShipping)
+        )
+      }
+      waitForElement('.new_shipping_block').then(() => {
+        $$el('.new_shipping_block label').forEach(el => {
+          el.addEventListener('click', e => {
+            console.dir(el.previousElementSibling.getAttribute('id'))
+            if (el.previousElementSibling.getAttribute('id') === 'expeditedShippingVar') {
+              $el('.sum-row.shipping [name="shipping-option-1"]').click()
+            } else {
+              $el('.sum-row.shipping [name="shipping-option-0"]').click()
+            }
+          })
+        })
+      })
+    })
+  }
+  handleDisabledRadioBlock() {
+    if (!$el('.new_shipping_block') && !$el('.form-column-left button')) return
+
+    if ($el('.form-column-left button')?.disabled) {
+      $el('.new_shipping_block').style.opacity = '0.5'
+      $el('.new_shipping_block').style.pointerEvents = 'none'
+    } else {
+      $el('.new_shipping_block').style.opacity = '1'
+      $el('.new_shipping_block').style.pointerEvents = 'auto'
+    }
   }
 }
 
