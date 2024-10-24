cded1904c9a46e890430cbed9636a366dc2c54a3
Svetlana Khvastova <svetakhvastova@gmail.com>
2024-10-24 18:27:32 +0300

    fix

--- goodevas/new_pdp/blocks.ts
+++ goodevas/new_pdp/blocks.ts
@@ -1,4 +1,85 @@
-import { svg, git, translations } from './data'
+import { svg, git, productDetailsTxt } from './data'
+
+export const bestSellerLabelBlock = /* HTML */ ` <div class="best_seller_label_block">${svg.bestSellerLabelIcon}</div> `
+
+export const boughtSoFarBlock = (summ: string = ''): string => {
+  return /* HTML */ `
+    <div class="bought_so_far_block">
+      ${svg.cartIcon}
+      <p><span class="">3K+</span> bought so far</p>
+    </div>
+  `
+}
+
+export const getFreeDeliveryBlock = () => {
+  return /* HTML */ `
+    <div class="get_free_delivery_block">
+      ${svg.freeDeliveryIcon}
+      <p>
+        Order now and get <span class="accent_underline">FREE delivery</span> on or before
+        <span class="date_txt">Sunday, October 19</span>
+      </p>
+    </div>
+  `
+}
+
+export const yourImpactBlock = () => {
+  return /* HTML */ `
+    <div class="your_impact_block">
+      ${svg.treeIcon}
+      <p>Your Impact: 5 trees planted with this order</p>
+    </div>
+  `
+}
+
+export const oneReviewBlock = () => {
+  return /* HTML */ `
+    <div class="one_review_block">
+      <div class="info_wrapper">
+        <p class="info_descr">
+          “Amazing quality of the product. Very easy to assemble. Both of my kids love it! I’m really happy with our
+          purchase. Definitely happy and will purchase again next time.”
+        </p>
+        <div class="name_stars_wrapper">
+          <span class="name_review">Courtney D.</span>
+          <div class="stars_summary">
+            <div class="stars_wrapper">${svg.starIcon}${svg.starIcon}${svg.starIcon}${svg.starIcon}${svg.starIcon}</div>
+            <span>5</span>
+          </div>
+        </div>
+      </div>
+      <div class="img_wrapper">
+        <img src="" alt="photo product" />
+        <a href="#" class="all_reviews_link">All reviews</a>
+      </div>
+    </div>
+  `
+}
+
+export const productDetailsBlock = () => {
+  return /* HTML */ `
+    <div class="product_details_block">
+      <h2>Product details</h2>
+      <ul class="product_details_accordion">
+        ${productDetailsTxt
+          .map((q, i) => {
+            return /* HTML */ `
+              <li class="product_details_accordion_block" data-visability="${i + 1}">
+                <div class="product_details_accordion_link">
+                  <p>${q.title}</p>
+                  <span class="product_details_icon">${svg.arrowAccordionIcon}</span>
+                </div>
+                <div class="product_details_accordion_lists" data-visability-open="" ${i + 1}>
+                  <div>${q.txt}</div>
+                </div>
+              </li>
+            `
+          })
+          .join('')}
+      </ul>
+    </div>
+  `
+}
 
 interface Benefit {
   icon: string
@@ -49,7 +130,7 @@ export const comparisonTableBlock = (data): string => {
           <div class="header_table">
             <div class="table_cell">
               <h3>${data.goodevasTitle}</h3>
-              <img src="${data.goodevasImg}" alt="Goodevas product" />
+              <div class="new_img"></div>
             </div>
           </div>
           <div class="body_table">
@@ -70,7 +151,7 @@ export const comparisonTableBlock = (data): string => {
           <div class="header_table">
             <div class="table_cell">
               <h3>${data.competitorsTitle}</h3>
-              <img src="${data.competitorsImg}" alt="Competitors product" />
+              <div class="new_img"></div>
             </div>
           </div>
           <div class="body_table">
--- goodevas/new_pdp/index.ts
+++ goodevas/new_pdp/index.ts
@@ -8,7 +8,16 @@ import {
   loadScriptsOrStyles,
   visibilityOfTime
 } from '../../libraries'
-import { comparisonTableBlock, mainBenefitsBlock } from './blocks'
+import {
+  bestSellerLabelBlock,
+  boughtSoFarBlock,
+  comparisonTableBlock,
+  getFreeDeliveryBlock,
+  mainBenefitsBlock,
+  oneReviewBlock,
+  productDetailsBlock,
+  yourImpactBlock
+} from './blocks'
 import { svg, git, translations } from './data'
 // @ts-ignore
 import mainStyle from './main.css?raw'
@@ -36,8 +45,121 @@ class NewPdp {
     )
     document.head.insertAdjacentHTML('beforeend', `<style class="crs_style">${mainStyle}</style>`)
 
+    this.renderBestSellerLabelForPhoto()
+    this.renderBoughtSoFarBlock()
+    this.renderGetFreeDeliveryBlock()
+    this.renderYourImpactBlock()
+    this.renderOneReviewBlock()
+    this.renderProductDetailsBlock()
+    this.initAccordionProductDetails()
+    this.toggleSeeMoreTxt()
     this.renderMainBenefits()
     this.renderComparisonTable()
+  }
+
+  renderBestSellerLabelForPhoto() {
+    waitForElement('.product__main-photos').then(i => {
+      const сontainerElement = $el('.product__main-photos') as HTMLElement
+
+      if (!$el('.best_seller_label_block')) {
+        сontainerElement.insertAdjacentHTML('afterbegin', bestSellerLabelBlock)
+      }
+    })
+  }
+
+  renderBoughtSoFarBlock() {
+    waitForElement('#shopify-block-yotpo_product_reviews_ugc_star_rating_FVceyX').then(i => {
+      const сontainerElement = $el('#shopify-block-yotpo_product_reviews_ugc_star_rating_FVceyX') as HTMLElement
+
+      if (!$el('.bought_so_far_block')) {
+        сontainerElement.insertAdjacentHTML('beforeend', boughtSoFarBlock())
+      }
+    })
+  }
+
+  renderGetFreeDeliveryBlock() {
+    waitForElement('block-price').then(i => {
+      const сontainerElement = $el('block-price') as HTMLElement
+
+      if (!$el('.get_free_delivery_block')) {
+        сontainerElement.insertAdjacentHTML('afterend', getFreeDeliveryBlock())
+      }
+    })
+  }
+
+  renderYourImpactBlock() {
+    waitForElement('block-buy-buttons').then(i => {
+      const сontainerElement = $el('block-buy-buttons') as HTMLElement
+
+      if (!$el('.your_impact_block')) {
+        сontainerElement.insertAdjacentHTML('afterend', yourImpactBlock())
+      }
+    })
+  }
+
+  renderOneReviewBlock() {
+    waitForElement('.your_impact_block').then(i => {
+      const сontainerElement = $el('.your_impact_block') as HTMLElement
+
+      if (!$el('.one_review_block')) {
+        сontainerElement.insertAdjacentHTML('afterend', oneReviewBlock())
+      }
+    })
+  }
+
+  renderProductDetailsBlock() {
+    waitForElement('.main_benefits_block').then(i => {
+      const сontainerElement = $el('.main_benefits_block') as HTMLElement
+
+      if (!$el('.product_details_block')) {
+        сontainerElement.insertAdjacentHTML('beforebegin', productDetailsBlock())
+      }
+    })
+  }
+
+  initAccordionProductDetails() {
+    let lookForJquery = setInterval(() => {
+      if (typeof jQuery === 'function' && $el('.product_details_block')) {
+        clearInterval(lookForJquery)
+        console.log('product_details_block')
+        $('.product_details_accordion_block').eq(0).addClass('active')
+        $('.product_details_accordion_block .product_details_accordion_link').eq(0).addClass('active')
+        $('.product_details_accordion_block .product_details_accordion_lists').eq(0).css('display', 'flex')
+
+        $('.product_details_accordion_link').on('click', function (e: any) {
+          // pushData(
+          //   'exp_pdp_improve_accordion_01',
+          //   `${e.currentTarget.querySelector('p').textContent}`,
+          //   'Accordion',
+          //   'Step 3: Device Details Frequently Asked Questions'
+          // )
+          $(this).toggleClass('active')
+          $(this).closest('li').toggleClass('active')
+          $(this).next('.product_details_accordion_lists').slideToggle()
+
+          $('.product_details_accordion_link').not(this).next('.product_details_accordion_lists').slideUp()
+          $('.product_details_accordion_link').not(this).removeClass('active').closest('li').removeClass('active')
+        })
+      }
+    }, 1000)
+  }
+
+  toggleSeeMoreTxt() {
+    waitForElement('.see_more_block').then(i => {
+      const seeMoreBtn = $el('.see_more_block') as HTMLElement
+
+      seeMoreBtn.addEventListener('click', e => {
+        if (e.currentTarget && !e.currentTarget.classList.contains('is_open')) {
+          e.currentTarget.classList.add('is_open')
+          e.currentTarget.querySelector('div').textContent = 'See Less'
+          e.currentTarget.previousElementSibling.classList.remove('blur_txt')
+        } else {
+          e.currentTarget.classList.remove('is_open')
+          e.currentTarget.querySelector('div').textContent = 'See More'
+          e.currentTarget.previousElementSibling.classList.add('blur_txt')
+        }
+      })
+    })
   }
 
   renderMainBenefits() {
